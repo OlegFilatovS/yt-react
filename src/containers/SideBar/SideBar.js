1b299240a2c45bf8178264448fb46e3911a7efe9
Planb <joincs@yandex.ru>
2020-01-30 21:11:05 +0300

    Finished sidebar componen

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Menu,Divider} from "semantic-ui-react";
 import './SideBar.scss';
 import {SideBarItem} from "./SideBarItem/SideBarItem";
+import {SideBarHeader} from "./SideBarHeader/SideBarHeader";
+import {Subscriptions} from "./Subscriptions/Subscriptions";
+import {SideBarFooter} from "./SideBarFooter/SideBarFooter";
 
 export class SideBar extends React.Component {
     render() {
@@ -11,15 +14,20 @@ export class SideBar extends React.Component {
                 <SideBarItem label="Trending" icon='fire'/>
                 <SideBarItem label="Followers" icon='spy'/>
                 <Divider/>
+                <SideBarHeader title="Library"/>
                 <SideBarItem label='History' icon='history'/>
                 <SideBarItem label='Watch later' icon='clock'/>
                 <SideBarItem label='Liked videos' icon='thumbs up'/>
                 <Divider/>
+                <Subscriptions/>
+                <SideBarHeader title="More from Youtube"/>
                 <SideBarItem label='Movies and Shows' icon='film'/>
                 <Divider/>
                 <SideBarItem label='Report history' icon='flag'/>
                 <SideBarItem label='Help' icon='help circle'/>
                 <SideBarItem label='Send feedback' icon='comment'/>
+                <Divider/>
+                <SideBarFooter/>
             </Menu>
         )
     }
