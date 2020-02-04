6fba191b1a8ad8d5fbdac1047661d423fb74c854
OlegF <oleg.filatov@gs-labs.ru>
2020-02-04 21:23:43 +0300

    added Preview and Home containers

---
+++ src/components/VideoPreview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./VideoPreview.scss";
+
+export class VideoPreview extends React.Component {
+        render() {
+            return(
+            <div> VideoPreview placeholder </div>
+            );
+        }
+};
